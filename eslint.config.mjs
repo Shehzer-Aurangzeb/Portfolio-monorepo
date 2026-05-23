21f6990af29e28ef525c63665abd8f49f02a5463
Shehzar Abbasi <shehzerabbasi621@gmail.com>
2026-05-22 20:34:31 -0400

    chore: initial monorepo setup with Next.js and Sanity

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/.next/**', '**/dist/**'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs['recommended'].rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
